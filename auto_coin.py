68cbf5b617a552756935d082ff2981aa6a9c53cf
KaiwenGong <kaiwen.gong@ucdconnect.ie>
2021-05-13 12:28:33 +0000

    2021-5-13_version_auto_sign_and_drop_coin

--- auto_coin.py
+++ auto_coin.py
@@ -8,10 +8,12 @@ import random
 import re
 import time
 import requests
+
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36"
     ,"Referer": "https://www.bilibili.com/",
 }
+
 def get_coin(sess):
     se,cs,duid = handle_info(sess);
     mycookies = {"SESSDATA":se,"bili_jct":cs,"DedeUserID": duid}
@@ -21,7 +23,22 @@ def get_coin(sess):
     b_j = mycookies["bili_jct"]
     url = "https://api.bilibili.com/x/web-interface/nav?build=0&mobi_app=web"
     content = S.get(url)
-    return int(json.loads(content.text)["data"]["money"])
+    return int(json.loads(content.text)["data"]['money'])
+
+def get_user_detail(uid):  # need user to enter the ID of the video producer
+    url = 'https://api.bilibili.com/x/relation/stat?vmid={}&jsonp=jsonp%20'.format(uid)
+    res = requests.get(url)
+    json_data = json.loads(res.text)
+    list = []
+    list.append(['UID: %s' % uid])
+    list.append(['He/She followers: %s'%json_data['data']['follower']])
+    list.append(['He/She follows: %s people'%json_data['data']['following']])
+    list.append(["There are %s people on his/her blacklist"%json_data['data']['black']])
+    string = ""
+    for l in list:
+        string = string+l[0]+","
+    return string
+
 def get_up_video(UID):
     a = []
     url = 'https://api.bilibili.com/x/space/arc/search?mid={}&ps=50&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp'.format(UID)
@@ -32,22 +49,28 @@ def get_up_video(UID):
         x = x + 1
         a.append(i['aid'])
     return a,x
+
 def handle_info(sess):
     list = re.split('=|;', sess)
-    i = 0
+    j = 0
+    while j < len(list):
+        list[j] = list[j].replace(" ","")
+        j = j + 1
     sess = ""
     cs = ""
     duid = ""
     se = ""
+    i = 0
     while i < len(list):
-        if list[i] == " SESSDATA":
+        if list[i] == "SESSDATA":
             se = list[i + 1];
-        if list[i] == " bili_jct":
+        if list[i] == "bili_jct":
             cs = list[i + 1]
-        if list[i] == " DedeUserID":
+        if list[i] == "DedeUserID":
             duid = list[i + 1]
         i = i + 1
     return se,cs,duid
+
 def auto_coin_like(sess, aid) :
     se,cs,duid = handle_info(sess);
     mycookies = {"SESSDATA":se,"bili_jct":cs,"DedeUserID": duid}
@@ -65,29 +88,35 @@ def auto_coin_like(sess, aid) :
     }
     content = S.post(url, post_data)
     text = json.loads(content.text)
-    print(text)
+    print(text["message"])
     # print(se+" "+sess+" "+duid)
-    return text["message"]
-def random_like_coin(sess,uid,coins):
+    # return text["message"]["money"]
 
+def random_like_coin(sess,uid,coins):
     list_of_video,x = get_up_video(uid)
     i = 0
     j = 0
-    while i < coins and get_coin(sess) > 0:
-        k = random.randint(0, x-1)
-        retrun = auto_coin_like(sess,list_of_video[k])
-        auto_Forwarding(sess, list_of_video[k])
-        print("the video the you try to drop coin and share : ")
-        print(list_of_video[k])
-        if retrun != '超过投币上限啦~':
-            i = i + 1
-        j = j + 1
-        if j > 30:
-            print("change a user please")
-            return
-        time.sleep(2)
+    if get_coin(sess) > coins:
+        while i < coins:
+            k = random.randint(0, x-1)
+            retrun = auto_coin_like(sess,list_of_video[k])
+            auto_Forwarding(sess, list_of_video[k])
+            print("the video the you try to drop coin and share : ")
+            print(list_of_video[k])
+            if retrun != '超过投币上限啦~':
+                i = i + 1
+            j = j + 1
+            if j > 30:
+                print("maybe you have drop too many coins to his videos, change a user please")
+                return get_user_detail(uid)
+            time.sleep(2)
+        else:
+            print("you do not have enough coin")
+            return get_user_detail(uid)
+    return get_user_detail(uid)
+
 def auto_Forwarding(sess,aid):
-    se, cs, duid = handle_info(sess);
+    se, cs, duid = handle_info(sess)
     url = "https://api.bilibili.com/x/web-interface/share/add"
     post_data = {
         "aid": aid,
@@ -100,6 +129,6 @@ def auto_Forwarding(sess,aid):
     content = S.post(url, post_data)
     return json.loads(content.text)
 
-random_like_coin("_uuid=33983BE8-3B5E-31DE-84A9-82D37E22D32608283infoc; buvid3=B337C198-9EB0-4174-A981-14D455D2A338184997infoc; fingerprint3=bebaf37e711128ee5f5512f79e445037; buvid_fp=B337C198-9EB0-4174-A981-14D455D2A338184997infoc; CURRENT_FNVAL=80; SESSDATA=b2ed4dde,1631859958,5851d*31; bili_jct=352c7187c835e066bb4ba78c929f6b47; DedeUserID=10548896; DedeUserID__ckMd5=d323e304c5795812; sid=6ewg8zhf; buvid_fp_plain=B337C198-9EB0-4174-A981-14D455D2A338184997infoc; blackside_state=1; rpdid=|(k|mJmYJ~|l0J'uYuYJuY~uJ; LIVE_BUVID=AUTO8516193485120705; fingerprint=b941082b09b290aea778334025424485; fingerprint_s=7de1a2a0858ae1bac94c76bd7e88e8aa; PVID=3; bfe_id=fdfaf33a01b88dd4692ca80f00c2de7f",3301695,1)
+print(random_like_coin("_uuid=33983BE8-3B5E-31DE-84A9-82D37E22D32608283infoc; buvid3=B337C198-9EB0-4174-A981-14D455D2A338184997infoc; fingerprint3=bebaf37e711128ee5f5512f79e445037; buvid_fp=B337C198-9EB0-4174-A981-14D455D2A338184997infoc; CURRENT_FNVAL=80; SESSDATA=b2ed4dde,1631859958,5851d*31; bili_jct=352c7187c835e066bb4ba78c929f6b47; DedeUserID=10548896; DedeUserID__ckMd5=d323e304c5795812; sid=6ewg8zhf; buvid_fp_plain=B337C198-9EB0-4174-A981-14D455D2A338184997infoc; blackside_state=1; rpdid=|(k|mJmYJ~|l0J'uYuYJuY~uJ; LIVE_BUVID=AUTO8516193485120705; fingerprint=b941082b09b290aea778334025424485; fingerprint_s=7de1a2a0858ae1bac94c76bd7e88e8aa; PVID=3; bfe_id=1e33d9ad1cb29251013800c68af42315",3301695,3))
 # print(auto_Forwarding("_uuid=33983BE8-3B5E-31DE-84A9-82D37E22D32608283infoc; buvid3=B337C198-9EB0-4174-A981-14D455D2A338184997infoc; fingerprint3=bebaf37e711128ee5f5512f79e445037; buvid_fp=B337C198-9EB0-4174-A981-14D455D2A338184997infoc; CURRENT_FNVAL=80; SESSDATA=b2ed4dde,1631859958,5851d*31; bili_jct=352c7187c835e066bb4ba78c929f6b47; DedeUserID=10548896; DedeUserID__ckMd5=d323e304c5795812; sid=6ewg8zhf; buvid_fp_plain=B337C198-9EB0-4174-A981-14D455D2A338184997infoc; blackside_state=1; rpdid=|(k|mJmYJ~|l0J'uYuYJuY~uJ; LIVE_BUVID=AUTO8516193485120705; fingerprint=b941082b09b290aea778334025424485; fingerprint_s=7de1a2a0858ae1bac94c76bd7e88e8aa; PVID=3; bfe_id=1e33d9ad1cb29251013800c68af42315",374916041))
 # print(get_coin("_uuid=33983BE8-3B5E-31DE-84A9-82D37E22D32608283infoc; buvid3=B337C198-9EB0-4174-A981-14D455D2A338184997infoc; fingerprint3=bebaf37e711128ee5f5512f79e445037; buvid_fp=B337C198-9EB0-4174-A981-14D455D2A338184997infoc; CURRENT_FNVAL=80; SESSDATA=b2ed4dde,1631859958,5851d*31; bili_jct=352c7187c835e066bb4ba78c929f6b47; DedeUserID=10548896; DedeUserID__ckMd5=d323e304c5795812; sid=6ewg8zhf; buvid_fp_plain=B337C198-9EB0-4174-A981-14D455D2A338184997infoc; blackside_state=1; rpdid=|(k|mJmYJ~|l0J'uYuYJuY~uJ; LIVE_BUVID=AUTO8516193485120705; fingerprint=b941082b09b290aea778334025424485; fingerprint_s=7de1a2a0858ae1bac94c76bd7e88e8aa; PVID=3; bfe_id=1e33d9ad1cb29251013800c68af42315"))
